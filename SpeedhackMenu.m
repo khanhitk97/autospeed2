d1a23732887ff6a55731df8aadd4de30bd8473da
khanhitk97 <vdkhanh.22011997@gmail.com>
2026-08-18 11:35:55 +0700

    Update SpeedhackMenu.m

--- SpeedhackMenu.m
+++ SpeedhackMenu.m
@@ -1,267 +1,157 @@
 #import <UIKit/UIKit.h>
-#import <CoreGraphics/CoreGraphics.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
 extern void set_speed_factor(float factor);
-extern float get_speed_factor(void);
-
 #ifdef __cplusplus
 }
 #endif
 
 @interface SpeedhackMenu : UIView
-
-@property (nonatomic, strong) UIButton *mainButton;
-@property (nonatomic, strong) UIView *presetPanel;
+@property (nonatomic, strong) UIButton *mainBtn;
+@property (nonatomic, strong) UIView *panel;
 @property (nonatomic, strong) UIButton *btnOff;
 @property (nonatomic, strong) UIButton *btnOn;
-@property (nonatomic, assign) BOOL isExpanded;
 @property (nonatomic, strong) NSTimer *fadeTimer;
-@property (nonatomic, assign) BOOL isSpeedEnabled;
-
+@property (nonatomic, assign) BOOL isEnabled;
 @end
 
 @implementation SpeedhackMenu
 
 + (void)load {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = [self getKeyWindow];
-        if (keyWindow) {
-            SpeedhackMenu *menu = [[SpeedhackMenu alloc] initWithFrame:CGRectMake(20, 150, 50, 50)];
-            [keyWindow addSubview:menu];
-        }
-    });
-}
-
-+ (UIWindow *)getKeyWindow {
-    if (@available(iOS 13.0, *)) {
-        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
-                UIWindowScene *windowScene = (UIWindowScene *)scene;
-                for (UIWindow *window in windowScene.windows) {
-                    if (window.isKeyWindow) return window;
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
+                    for (UIWindow *w in ((UIWindowScene *)scene).windows) {
+                        if (w.isKeyWindow) { window = w; break; }
+                    }
                 }
             }
         }
-    }
-    return nil;
+        if (window) [window addSubview:[[SpeedhackMenu alloc] initWithFrame:CGRectMake(20, 150, 50, 50)]];
+    });
 }
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        _isExpanded = NO;
-        _isSpeedEnabled = YES; // Mặc định mở app là BẬT 5.0x
-        set_speed_factor(5.00f);
+        _isEnabled = YES; // Mặc định mở game là BẬT 5x
+        set_speed_factor(5.0f);
 
-        // 1. Nút bong bóng chính
-        _mainButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        _mainButton.frame = CGRectMake(0, 0, 50, 50);
-        _mainButton.layer.cornerRadius = 25.0;
-        _mainButton.layer.borderWidth = 2.0;
-        _mainButton.layer.borderColor = [UIColor whiteColor].CGColor;
-        _mainButton.layer.shadowColor = [UIColor blackColor].CGColor;
-        _mainButton.layer.shadowOffset = CGSizeMake(0, 2);
-        _mainButton.layer.shadowOpacity = 0.3;
-        _mainButton.layer.shadowRadius = 4.0;
-        
-        [self updateMainButtonTitle];
-        _mainButton.titleLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightBold];
-        [_mainButton addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:_mainButton];
+        // Nút tròn chính
+        _mainBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _mainBtn.frame = CGRectMake(0, 0, 50, 50);
+        _mainBtn.layer.cornerRadius = 25;
+        _mainBtn.layer.borderWidth = 2;
+        _mainBtn.layer.borderColor = [UIColor whiteColor].CGColor;
+        _mainBtn.titleLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightBold];
+        [_mainBtn addTarget:self action:@selector(toggleMenu) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:_mainBtn];
 
-        // Gesture kéo thả
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
-        [self addGestureRecognizer:pan];
+        // Kéo thả menu
+        [self addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)]];
 
-        // 2. Bảng điều khiển chọn 1 hoặc 2
-        _presetPanel = [[UIView alloc] initWithFrame:CGRectMake(55, 2.5, 175, 45)];
-        _presetPanel.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.14 alpha:0.95];
-        _presetPanel.layer.cornerRadius = 12.0;
-        _presetPanel.layer.borderWidth = 1.5;
-        _presetPanel.layer.borderColor = [UIColor colorWithRed:0.1 green:0.55 blue:1.0 alpha:1.0].CGColor;
-        _presetPanel.alpha = 0.0;
-        _presetPanel.hidden = YES;
+        // Thanh mở rộng
+        _panel = [[UIView alloc] initWithFrame:CGRectMake(55, 2.5, 175, 45)];
+        _panel.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.14 alpha:0.95];
+        _panel.layer.cornerRadius = 12;
+        _panel.layer.borderWidth = 1.5;
+        _panel.layer.borderColor = [UIColor colorWithRed:0.1 green:0.55 blue:1.0 alpha:1.0].CGColor;
+        _panel.hidden = YES;
+        _panel.alpha = 0.0;
 
-        // Nút 1: TẮT (1.0x)
-        _btnOff = [UIButton buttonWithType:UIButtonTypeCustom];
-        _btnOff.frame = CGRectMake(8, 7.5, 75, 30);
-        _btnOff.layer.cornerRadius = 8.0;
-        _btnOff.tag = 1;
-        [_btnOff setTitle:@"1: TẮT" forState:UIControlStateNormal];
-        _btnOff.titleLabel.font = [UIFont systemFontOfSize:12 weight:UIFontWeightBold];
-        [_btnOff addTarget:self action:@selector(actionButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
-        [_presetPanel addSubview:_btnOff];
+        _btnOff = [self makeBtn:@"1: TẮT" tag:1 frame:CGRectMake(8, 7.5, 75, 30)];
+        _btnOn = [self makeBtn:@"2: BẬT" tag:2 frame:CGRectMake(91, 7.5, 75, 30)];
+        [_panel addSubview:_btnOff];
+        [_panel addSubview:_btnOn];
+        [self addSubview:_panel];
 
-        // Nút 2: BẬT (5.0x)
-        _btnOn = [UIButton buttonWithType:UIButtonTypeCustom];
-        _btnOn.frame = CGRectMake(91, 7.5, 75, 30);
-        _btnOn.layer.cornerRadius = 8.0;
-        _btnOn.tag = 2;
-        [_btnOn setTitle:@"2: BẬT" forState:UIControlStateNormal];
-        _btnOn.titleLabel.font = [UIFont systemFontOfSize:12 weight:UIFontWeightBold];
-        [_btnOn addTarget:self action:@selector(actionButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
-        [_presetPanel addSubview:_btnOn];
-
-        [self updateButtonStates];
-        [self addSubview:_presetPanel];
-
+        [self updateUI];
         self.alpha = 0.15;
     }
     return self;
 }
 
-- (void)updateMainButtonTitle {
-    if (_isSpeedEnabled) {
-        [_mainButton setTitle:@"⚡️5x" forState:UIControlStateNormal];
-        _mainButton.backgroundColor = [UIColor colorWithRed:0.15 green:0.68 blue:0.38 alpha:0.9]; // Xanh lá
-    } else {
-        [_mainButton setTitle:@"OFF" forState:UIControlStateNormal];
-        _mainButton.backgroundColor = [UIColor colorWithRed:0.85 green:0.25 blue:0.20 alpha:0.9]; // Đỏ cam
-    }
+- (UIButton *)makeBtn:(NSString *)title tag:(NSInteger)tag frame:(CGRect)frame {
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = frame;
+    btn.tag = tag;
+    btn.layer.cornerRadius = 8;
+    btn.titleLabel.font = [UIFont systemFontOfSize:12 weight:UIFontWeightBold];
+    [btn setTitle:title forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnTapped:) forControlEvents:UIControlEventTouchUpInside];
+    return btn;
 }
 
-- (void)updateButtonStates {
-    if (_isSpeedEnabled) {
+- (void)updateUI {
+    if (_isEnabled) {
+        [_mainBtn setTitle:@"⚡️5x" forState:UIControlStateNormal];
+        _mainBtn.backgroundColor = [UIColor colorWithRed:0.15 green:0.68 blue:0.38 alpha:0.9];
         _btnOn.backgroundColor = [UIColor colorWithRed:0.15 green:0.68 blue:0.38 alpha:1.0];
         [_btnOn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-
         _btnOff.backgroundColor = [UIColor colorWithRed:0.22 green:0.22 blue:0.25 alpha:1.0];
         [_btnOff setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
     } else {
+        [_mainBtn setTitle:@"OFF" forState:UIControlStateNormal];
+        _mainBtn.backgroundColor = [UIColor colorWithRed:0.85 green:0.25 blue:0.20 alpha:0.9];
         _btnOff.backgroundColor = [UIColor colorWithRed:0.85 green:0.25 blue:0.20 alpha:1.0];
         [_btnOff setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-
         _btnOn.backgroundColor = [UIColor colorWithRed:0.22 green:0.22 blue:0.25 alpha:1.0];
         [_btnOn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
     }
 }
 
-- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
-    if (self.hidden || self.alpha < 0.01) return nil;
-
-    CGPoint mainPoint = [self convertPoint:point toView:self.mainButton];
-    if ([self.mainButton pointInside:mainPoint withEvent:event]) {
-        return self.mainButton;
-    }
-
-    if (self.isExpanded) {
-        CGPoint panelPoint = [self convertPoint:point toView:self.presetPanel];
-        if ([self.presetPanel pointInside:panelPoint withEvent:event]) {
-            return [self.presetPanel hitTest:panelPoint withEvent:event];
-        }
-    }
-    return nil;
-}
-
-- (void)triggerHaptic {
-    if (@available(iOS 10.0, *)) {
-        UIImpactFeedbackGenerator *generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleLight];
-        [generator prepare];
-        [generator impactOccurred];
-    }
-}
-
-- (void)actionButtonTapped:(UIButton *)sender {
-    [self triggerHaptic];
-    [self resetFadeTimer];
+- (void)btnTapped:(UIButton *)sender {
+    [self resetTimer];
     self.alpha = 1.0;
-
-    if (sender.tag == 1) {
-        // NÚT 1: TẮT -> Trả về 1.0x gốc
-        _isSpeedEnabled = NO;
-        set_speed_factor(1.00f);
-    } else if (sender.tag == 2) {
-        // NÚT 2: BẬT -> Kích hoạt 5.0x
-        _isSpeedEnabled = YES;
-        set_speed_factor(5.00f);
-    }
-
-    [self updateButtonStates];
-    [self updateMainButtonTitle];
+    _isEnabled = (sender.tag == 2);
+    set_speed_factor(_isEnabled ? 5.0f : 1.0f);
+    [self updateUI];
 }
 
 - (void)toggleMenu {
-    [self triggerHaptic];
-    [self resetFadeTimer];
+    [self resetTimer];
     self.alpha = 1.0;
-    
-    _isExpanded = !_isExpanded;
-    
-    if (_isExpanded) {
-        _presetPanel.hidden = NO;
-    }
+    BOOL willShow = _panel.hidden;
+    if (willShow) _panel.hidden = NO;
 
-    [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0.5 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-        if (self.isExpanded) {
-            self.presetPanel.alpha = 1.0;
-            self.presetPanel.transform = CGAffineTransformIdentity;
-        } else {
-            self.presetPanel.alpha = 0.0;
-            self.presetPanel.transform = CGAffineTransformMakeScale(0.8, 0.8);
-        }
-    } completion:^(BOOL finished) {
-        if (!self.isExpanded) {
-            self.presetPanel.hidden = YES;
-        }
+    [UIView animateWithDuration:0.25 animations:^{
+        self.panel.alpha = willShow ? 1.0 : 0.0;
+    } completion:^(BOOL f) {
+        if (!willShow) self.panel.hidden = YES;
     }];
 }
 
 - (void)handlePan:(UIPanGestureRecognizer *)pan {
-    [self resetFadeTimer];
+    [self resetTimer];
     self.alpha = 1.0;
-    
-    CGPoint translation = [pan translationInView:self.superview];
-    self.center = CGPointMake(self.center.x + translation.x, self.center.y + translation.y);
+    CGPoint trans = [pan translationInView:self.superview];
+    self.center = CGPointMake(self.center.x + trans.x, self.center.y + trans.y);
     [pan setTranslation:CGPointZero inView:self.superview];
 
-    if (pan.state == UIGestureRecognizerStateEnded || pan.state == UIGestureRecognizerStateCancelled) {
-        UIEdgeInsets insets = UIEdgeInsetsZero;
-        if (@available(iOS 11.0, *)) {
-            insets = self.superview.safeAreaInsets;
-        }
-        
-        CGFloat screenWidth = self.superview.bounds.size.width;
-        CGFloat screenHeight = self.superview.bounds.size.height;
-        CGFloat halfWidth = self.bounds.size.width / 2.0;
-        
-        CGFloat targetX = (self.center.x < screenWidth / 2.0) ? (insets.left + halfWidth + 10) : (screenWidth - insets.right - halfWidth - 10);
-        CGFloat targetY = MIN(MAX(self.center.y, insets.top + halfWidth + 10), screenHeight - insets.bottom - halfWidth - 10);
-        
-        if (targetX > screenWidth / 2.0) {
-            self.presetPanel.frame = CGRectMake(-180, 2.5, 175, 45);
-        } else {
-            self.presetPanel.frame = CGRectMake(55, 2.5, 175, 45);
-        }
-        
-        [UIView animateWithDuration:0.3 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0.5 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-            self.center = CGPointMake(targetX, targetY);
-        } completion:nil];
+    if (pan.state == UIGestureRecognizerStateEnded) {
+        CGFloat screenW = self.superview.bounds.size.width;
+        CGFloat targetX = (self.center.x < screenW / 2.0) ? 35 : (screenW - 35);
+        _panel.frame = (targetX > screenW / 2.0) ? CGRectMake(-180, 2.5, 175, 45) : CGRectMake(55, 2.5, 175, 45);
 
-        [self startFadeTimer];
+        [UIView animateWithDuration:0.25 animations:^{
+            self.center = CGPointMake(targetX, self.center.y);
+        }];
+        [self resetTimer];
     }
 }
 
-- (void)resetFadeTimer {
+- (void)resetTimer {
     [_fadeTimer invalidate];
-    _fadeTimer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(dimMenu) userInfo:nil repeats:NO];
+    _fadeTimer = [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(dim) userInfo:nil repeats:NO];
 }
 
-- (void)startFadeTimer {
-    [self resetFadeTimer];
-}
-
-- (void)dimMenu {
-    if (self.isExpanded) {
-        [self toggleMenu];
-    }
-    
-    [UIView animateWithDuration:0.5 animations:^{
-        self.alpha = 0.15;
-    }];
+- (void)dim {
+    if (!_panel.hidden) [self toggleMenu];
+    [UIView animateWithDuration:0.4 animations:^{ self.alpha = 0.15; }];
 }
 
 @end
