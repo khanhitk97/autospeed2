34e8aa3b3e169851ac108d0b0388de4081fb1cb5
khanhitk97 <vdkhanh.22011997@gmail.com>
2026-08-18 12:53:16 +0700

    Update SpeedhackMenu.m

--- SpeedhackMenu.m
+++ SpeedhackMenu.m
@@ -12,201 +12,75 @@ extern void set_speed_factor(float factor);
 #endif
 
 // ==========================================
-// 1. WEATHER HEADER VIEW (MÂY, TRỜI, TRĂNG, MƯA, TIA SÁNG)
+// 1. MINIMAL WEATHER HEADER (MÂY TRÔI NGÀY / ĐÊM)
 // ==========================================
-@interface WeatherHeaderView : UIView
-@property (nonatomic, strong) CAGradientLayer *skyGradient;
-@property (nonatomic, strong) UIView *sunOrMoonView;
-@property (nonatomic, strong) UIView *sunRaysView;
+@interface MinimalWeatherHeader : UIView
+@property (nonatomic, strong) UIView *nightTintOverlay;
 @property (nonatomic, strong) UIView *cloudContainer;
-@property (nonatomic, strong) CAEmitterLayer *rainEmitter;
-@property (nonatomic, strong) CAEmitterLayer *starEmitter;
+@property (nonatomic, strong) UIView *cloud1;
+@property (nonatomic, strong) UIView *cloud2;
 @end
 
-@implementation WeatherHeaderView
-
-- (UIImage *)generateRainParticle {
-    CGSize size = CGSizeMake(2, 12);
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGContextSetFillColorWithColor(ctx, [UIColor colorWithWhite:1.0 alpha:0.75].CGColor);
-    CGContextFillRect(ctx, CGRectMake(0, 0, 2, 12));
-    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return img;
-}
-
-- (UIImage *)generateStarParticle {
-    CGSize size = CGSizeMake(4, 4);
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGContextSetFillColorWithColor(ctx, [UIColor colorWithWhite:1.0 alpha:0.9].CGColor);
-    CGContextFillEllipseInRect(ctx, CGRectMake(0, 0, 4, 4));
-    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return img;
-}
+@implementation MinimalWeatherHeader
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.userInteractionEnabled = NO;
+        self.userInteractionEnabled = NO; // Không chặn cảm ứng của app
         self.clipsToBounds = YES;
 
-        // Bầu trời động (Sky Gradient)
-        _skyGradient = [CAGradientLayer layer];
-        _skyGradient.frame = self.bounds;
-        _skyGradient.startPoint = CGPointMake(0.0, 0.0);
-        _skyGradient.endPoint = CGPointMake(1.0, 1.0);
-        [self.layer addSublayer:_skyGradient];
+        // Lớp phủ tối dịu mắt ban đêm (mỏng nhẹ, giữ nguyên chữ)
+        _nightTintOverlay = [[UIView alloc] initWithFrame:self.bounds];
+        _nightTintOverlay.userInteractionEnabled = NO;
+        _nightTintOverlay.backgroundColor = [UIColor colorWithRed:0.04 green:0.06 blue:0.12 alpha:0.35];
+        [self addSubview:_nightTintOverlay];
 
-        // Container cho Mây & Gió
+        // Vùng chứa mây trôi
         _cloudContainer = [[UIView alloc] initWithFrame:self.bounds];
         _cloudContainer.userInteractionEnabled = NO;
         [self addSubview:_cloudContainer];
 
-        // Tia sáng / Hào quang
-        _sunRaysView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
-        _sunRaysView.userInteractionEnabled = NO;
-        [self addSubview:_sunRaysView];
+        _cloud1 = [self createCloudWithWidth:48 height:16];
+        _cloud2 = [self createCloudWithWidth:36 height:13];
 
-        // Mặt trời / Mặt trăng
-        _sunOrMoonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
-        _sunOrMoonView.userInteractionEnabled = NO;
-        _sunOrMoonView.layer.cornerRadius = 16.0;
-        [self addSubview:_sunOrMoonView];
+        [_cloudContainer addSubview:_cloud1];
+        [_cloudContainer addSubview:_cloud2];
 
-        // Hạt mưa (Rainfall Emitter)
-        _rainEmitter = [CAEmitterLayer layer];
-        _rainEmitter.emitterPosition = CGPointMake(frame.size.width / 2.0, -10);
-        _rainEmitter.emitterSize = CGSizeMake(frame.size.width * 1.5, 10);
-        _rainEmitter.emitterShape = kCAEmitterLayerLine;
-
-        CAEmitterCell *rainCell = [CAEmitterCell emitterCell];
-        rainCell.birthRate = 22;
-        rainCell.lifetime = 1.2;
-        rainCell.velocity = 260;
-        rainCell.velocityRange = 40;
-        rainCell.emissionLongitude = (CGFloat)(M_PI_2 + M_PI / 12.0); // Rơi xiên theo gió
-        rainCell.scale = 0.8;
-        rainCell.alphaSpeed = -0.4;
-        rainCell.contents = (id)[self generateRainParticle].CGImage;
-        _rainEmitter.emitterCells = @[rainCell];
-        [self.layer addSublayer:_rainEmitter];
-
-        // Sao nhấp nháy ban đêm
-        _starEmitter = [CAEmitterLayer layer];
-        _starEmitter.emitterPosition = CGPointMake(frame.size.width / 2.0, frame.size.height / 2.0);
-        _starEmitter.emitterSize = frame.size;
-        _starEmitter.emitterShape = kCAEmitterLayerRectangle;
-
-        CAEmitterCell *starCell = [CAEmitterCell emitterCell];
-        starCell.birthRate = 6;
-        starCell.lifetime = 3.0;
-        starCell.scale = 0.6;
-        starCell.scaleRange = 0.4;
-        starCell.alphaSpeed = -0.3;
-        starCell.contents = (id)[self generateStarParticle].CGImage;
-        _starEmitter.emitterCells = @[starCell];
-        [self.layer addSublayer:_starEmitter];
-
-        [self updateWeatherCycle];
-        [self startCloudAnimation];
+        [self updateDayNightState];
+        [self startClouds];
     }
     return self;
 }
 
-- (void)updateWeatherCycle {
+- (UIView *)createCloudWithWidth:(CGFloat)w height:(CGFloat)h {
+    UIView *cloud = [[UIView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
+    cloud.layer.cornerRadius = h / 2.0;
+    return cloud;
+}
+
+- (void)updateDayNightState {
     NSCalendar *calendar = [NSCalendar currentCalendar];
     NSInteger hour = [calendar component:NSCalendarUnitHour fromDate:[NSDate date]];
 
-    if (hour >= 6 && hour < 17) {
-        // BAN NGÀY: Nắng rực rỡ (Gradient Cam - Vàng)
-        _skyGradient.colors = @[
-            (id)[UIColor colorWithRed:1.0 green:0.42 blue:0.18 alpha:0.95].CGColor,
-            (id)[UIColor colorWithRed:1.0 green:0.62 blue:0.25 alpha:0.95].CGColor
-        ];
-
-        // Mặt trời
-        _sunOrMoonView.center = CGPointMake(self.bounds.size.width - 45, 52);
-        _sunOrMoonView.backgroundColor = [UIColor colorWithRed:1.0 green:0.92 blue:0.4 alpha:1.0];
-        _sunOrMoonView.layer.shadowColor = [UIColor colorWithRed:1.0 green:0.85 blue:0.2 alpha:1.0].CGColor;
-        _sunOrMoonView.layer.shadowRadius = 12.0;
-        _sunOrMoonView.layer.shadowOpacity = 0.95;
-
-        // Tia sáng mặt trời quay chậm
-        _sunRaysView.center = _sunOrMoonView.center;
-        _sunRaysView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.15];
-        _sunRaysView.layer.cornerRadius = 40.0;
-        [self startRaysRotation];
-
-        _starEmitter.birthRate = 0;
-        _rainEmitter.birthRate = 0;
-
-    } else if (hour >= 17 && hour < 19) {
-        // HOÀNG HÔN: Gradient Tím - Đỏ Cam
-        _skyGradient.colors = @[
-            (id)[UIColor colorWithRed:0.85 green:0.25 blue:0.35 alpha:0.95].CGColor,
-            (id)[UIColor colorWithRed:0.98 green:0.48 blue:0.22 alpha:0.95].CGColor
-        ];
-
-        _sunOrMoonView.center = CGPointMake(self.bounds.size.width - 45, 62);
-        _sunOrMoonView.backgroundColor = [UIColor colorWithRed:1.0 green:0.4 blue:0.2 alpha:1.0];
-        _sunOrMoonView.layer.shadowColor = [UIColor colorWithRed:1.0 green:0.3 blue:0.1 alpha:0.9].CGColor;
-        _sunOrMoonView.layer.shadowRadius = 14.0;
-        _sunOrMoonView.layer.shadowOpacity = 0.9;
-
-        _sunRaysView.hidden = YES;
-        _starEmitter.birthRate = 0;
-        _rainEmitter.birthRate = 0;
-
+    if (hour >= 6 && hour < 18) {
+        // BAN NGÀY: Ẩn lớp phủ đêm, mây trắng bồng bềnh
+        _nightTintOverlay.hidden = YES;
+        _cloud1.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.32];
+        _cloud2.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.25];
     } else {
-        // BAN ĐÊM: Gradient Xanh Đen Midnight + Ánh Cam Đáy
-        _skyGradient.colors = @[
-            (id)[UIColor colorWithRed:0.08 green:0.10 blue:0.18 alpha:0.96].CGColor,
-            (id)[UIColor colorWithRed:0.18 green:0.14 blue:0.24 alpha:0.96].CGColor
-        ];
-
-        // Mặt trăng khuyết phát sáng bạc dịu
-        _sunOrMoonView.center = CGPointMake(self.bounds.size.width - 45, 50);
-        _sunOrMoonView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:0.95];
-        _sunOrMoonView.layer.shadowColor = [UIColor colorWithRed:0.6 green:0.8 blue:1.0 alpha:0.8].CGColor;
-        _sunOrMoonView.layer.shadowRadius = 10.0;
-        _sunOrMoonView.layer.shadowOpacity = 0.9;
-
-        _sunRaysView.hidden = YES;
-        _starEmitter.birthRate = 5;
-        _rainEmitter.birthRate = 18; // Mưa đêm li ti mờ ảo
+        // BAN ĐÊM: Hiện lớp phủ đêm dịu mắt, mây ánh xám bạc
+        _nightTintOverlay.hidden = NO;
+        _cloud1.backgroundColor = [UIColor colorWithWhite:0.85 alpha:0.22];
+        _cloud2.backgroundColor = [UIColor colorWithWhite:0.85 alpha:0.18];
     }
 }
 
-- (void)startRaysRotation {
-    CABasicAnimation *rotate = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-    rotate.toValue = @(M_PI * 2.0);
-    rotate.duration = 18.0;
-    rotate.repeatCount = HUGE_VALF;
-    [_sunRaysView.layer addAnimation:rotate forKey:@"sunRays"];
-}
+- (void)startClouds {
+    _cloud1.frame = CGRectMake(-60, 42, 48, 16);
+    _cloud2.frame = CGRectMake(-90, 58, 36, 13);
 
-// Mây bồng bềnh trôi ngang theo gió
-- (void)startCloudAnimation {
-    UIView *cloud1 = [self createCloudViewWithWidth:50 height:18];
-    cloud1.frame = CGRectMake(-60, 38, 50, 18);
-    [_cloudContainer addSubview:cloud1];
-
-    UIView *cloud2 = [self createCloudViewWithWidth:38 height:14];
-    cloud2.frame = CGRectMake(-100, 56, 38, 14);
-    [_cloudContainer addSubview:cloud2];
-
-    [self animateCloud:cloud1 duration:22.0 delay:0];
-    [self animateCloud:cloud2 duration:28.0 delay:5.0];
-}
-
-- (UIView *)createCloudViewWithWidth:(CGFloat)w height:(CGFloat)h {
-    UIView *cloud = [[UIView alloc] initWithFrame:CGRectMake(0, 0, w, h)];
-    cloud.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.35];
-    cloud.layer.cornerRadius = h / 2.0;
-    return cloud;
+    [self animateCloud:_cloud1 duration:36.0 delay:0];
+    [self animateCloud:_cloud2 duration:46.0 delay:8.0];
 }
 
 - (void)animateCloud:(UIView *)cloud duration:(NSTimeInterval)dur delay:(NSTimeInterval)del {
@@ -227,12 +101,13 @@ extern void set_speed_factor(float factor);
 @end
 
 // ==========================================
-// 2. FLY VIEW (BÉ RUỒI ĐẬP CÁNH & BAY LƯỢN)
+// 2. FLY VIEW (RUỒI PHÁT SÁNG ĐOM ĐÓM BAN ĐÊM)
 // ==========================================
 @interface FlyView : UIView
 @property (nonatomic, strong) UIView *leftWing;
 @property (nonatomic, strong) UIView *rightWing;
 @property (nonatomic, strong) UIView *bodyView;
+@property (nonatomic, strong) UIView *glowTail;
 @end
 
 @implementation FlyView
@@ -242,10 +117,20 @@ extern void set_speed_factor(float factor);
     if (self) {
         self.userInteractionEnabled = NO;
 
+        // Đốm dạ quang phát sáng ở đuôi (Chỉ hiện ban đêm)
+        _glowTail = [[UIView alloc] initWithFrame:CGRectMake(9.5, 12, 5, 8)];
+        _glowTail.backgroundColor = [UIColor colorWithRed:0.4 green:1.0 blue:0.2 alpha:0.95];
+        _glowTail.layer.cornerRadius = 2.5;
+        _glowTail.layer.shadowColor = [UIColor colorWithRed:0.3 green:1.0 blue:0.2 alpha:1.0].CGColor;
+        _glowTail.layer.shadowRadius = 8.0;
+        _glowTail.layer.shadowOpacity = 0.95;
+
+        // Thân ruồi
         _bodyView = [[UIView alloc] initWithFrame:CGRectMake(9, 3, 6, 18)];
         _bodyView.backgroundColor = [UIColor colorWithRed:0.12 green:0.12 blue:0.12 alpha:0.95];
         _bodyView.layer.cornerRadius = 3.0;
 
+        // Mắt ruồi
         UIView *leftEye = [[UIView alloc] initWithFrame:CGRectMake(8, 2, 2.5, 2.5)];
         leftEye.backgroundColor = [UIColor colorWithRed:0.75 green:0.1 blue:0.1 alpha:0.9];
         leftEye.layer.cornerRadius = 1.25;
@@ -254,6 +139,7 @@ extern void set_speed_factor(float factor);
         rightEye.backgroundColor = [UIColor colorWithRed:0.75 green:0.1 blue:0.1 alpha:0.9];
         rightEye.layer.cornerRadius = 1.25;
 
+        // Cánh mỏng
         _leftWing = [[UIView alloc] initWithFrame:CGRectMake(1, 6, 9, 14)];
         _leftWing.backgroundColor = [UIColor colorWithWhite:0.95 alpha:0.65];
         _leftWing.layer.cornerRadius = 4.5;
@@ -268,16 +154,39 @@ extern void set_speed_factor(float factor);
         _rightWing.layer.borderWidth = 0.5;
         _rightWing.layer.borderColor = [UIColor colorWithWhite:0.7 alpha:0.4].CGColor;
 
+        [self addSubview:_glowTail];
         [self addSubview:_leftWing];
         [self addSubview:_rightWing];
         [self addSubview:_bodyView];
         [self addSubview:leftEye];
         [self addSubview:rightEye];
 
+        [self updateGlowState];
         [self startWingFlapping];
         [self startAI];
     }
     return self;
+}
+
+- (void)updateGlowState {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger hour = [calendar component:NSCalendarUnitHour fromDate:[NSDate date]];
+
+    if (hour >= 6 && hour < 18) {
+        // BAN NGÀY: Ẩn phát sáng
+        _glowTail.hidden = YES;
+        [_glowTail.layer removeAnimationForKey:@"glowPulse"];
+    } else {
+        // BAN ĐÊM: Bật phát sáng đom đóm + hiệu ứng nhấp nháy thở
+        _glowTail.hidden = NO;
+        CABasicAnimation *pulse = [CABasicAnimation animationWithKeyPath:@"shadowOpacity"];
+        pulse.fromValue = @(0.4);
+        pulse.toValue = @(1.0);
+        pulse.duration = 1.2;
+        pulse.autoreverses = YES;
+        pulse.repeatCount = HUGE_VALF;
+        [_glowTail.layer addAnimation:pulse forKey:@"glowPulse"];
+    }
 }
 
 - (void)startWingFlapping {
@@ -339,11 +248,11 @@ extern void set_speed_factor(float factor);
 @end
 
 // ==========================================
-// 3. SPEEDHACK MANAGER (ĐỒNG BỘ HIỆU ỨNG VÀ GESTURE)
+// 3. SPEEDHACK MANAGER
 // ==========================================
 @interface SpeedhackManager : NSObject <UIGestureRecognizerDelegate>
 @property (nonatomic, strong) FlyView *fly;
-@property (nonatomic, strong) WeatherHeaderView *weatherHeader;
+@property (nonatomic, strong) MinimalWeatherHeader *weatherHeader;
 @property (nonatomic, assign) BOOL isSpeedEnabled;
 @end
 
@@ -379,16 +288,16 @@ static SpeedhackManager *sharedManager = nil;
     _isSpeedEnabled = YES;
     set_speed_factor(5.0f);
 
-    // 1. Khởi tạo Weather Header bao phủ đúng vùng thanh tiêu đề cam
-    CGFloat headerHeight = 98.0; // Chiều cao chuẩn bao phủ từ tai thỏ tới mép dưới thanh cam
-    _weatherHeader = [[WeatherHeaderView alloc] initWithFrame:CGRectMake(0, 0, window.bounds.size.width, headerHeight)];
+    // 1. Header mây trôi bao phủ dải tiêu đề
+    CGFloat headerHeight = 98.0;
+    _weatherHeader = [[MinimalWeatherHeader alloc] initWithFrame:CGRectMake(0, 0, window.bounds.size.width, headerHeight)];
     [window addSubview:_weatherHeader];
 
-    // 2. Khởi tạo Bé Ruồi
+    // 2. Bé Ruồi
     _fly = [[FlyView alloc] initWithFrame:CGRectMake(window.bounds.size.width / 2.0, window.bounds.size.height / 2.0, 24, 24)];
     [window addSubview:_fly];
 
-    // 3. Gắn cử chỉ chạm 3 ngón 2 lần
+    // 3. Cử chỉ chạm 3 ngón 2 lần
     UITapGestureRecognizer *tripleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSecretGesture:)];
     tripleTap.numberOfTouchesRequired = 3;
     tripleTap.numberOfTapsRequired = 2;
@@ -419,7 +328,6 @@ static SpeedhackManager *sharedManager = nil;
     }
 
     if (_isSpeedEnabled) {
-        // BẬT: 5.0x + Hiện Bé Ruồi + Kích hoạt Weather Header
         set_speed_factor(5.0f);
 
         _weatherHeader.hidden = NO;
@@ -430,11 +338,11 @@ static SpeedhackManager *sharedManager = nil;
             self.fly.alpha = 1.0;
             self.fly.transform = CGAffineTransformIdentity;
         }];
-        [_weatherHeader updateWeatherCycle];
+        [_weatherHeader updateDayNightState];
+        [_fly updateGlowState];
         [_fly scheduleNextMove];
 
     } else {
-        // TẮT: 1.0x chuẩn gốc + Ẩn hoàn toàn Weather Header & Bé Ruồi
         set_speed_factor(1.0f);
 
         [UIView animateWithDuration:0.3 animations:^{
